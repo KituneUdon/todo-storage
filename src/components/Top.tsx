071df048645196480984f356c366b49e84a22ca7
KituneUdon <kituneudon97@gmail.com>
2021-03-29 17:03:58 +0900

    #1 トップ画面を作成

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,5 +1,23 @@
 import React, { FC } from 'react';
+import { Button } from '@material-ui/core';
+import { css } from '@emotion/css';
 
-const Top: FC = () => <div>Topページ</div>;
+const container = css`
+  width: 100%;
+  text-align: center;
+`;
+
+const Top: FC = () => (
+  <>
+    <div className={container}>
+      <h1>すべてのToDoを１箇所に</h1>
+    </div>
+    <div className={container}>
+      <Button variant="contained" color="primary">
+        開始
+      </Button>
+    </div>
+  </>
+);
 
 export default Top;
