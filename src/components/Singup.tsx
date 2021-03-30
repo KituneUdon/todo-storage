245315623758625487e69a0dc211aacd5d382ba6
KituneUdon <kituneudon97@gmail.com>
2021-03-30 15:21:24 +0900

    #1 アカウント作成画面を作成

--- src/components/Singup.tsx
+++ src/components/Singup.tsx
@@ -1,5 +1,35 @@
 import React, { FC } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { css } from '@emotion/css';
+import { Link } from 'react-router-dom';
 
-const Singup: FC = () => <div>アカウント作成画面</div>;
+const container = css`
+  width=100%;
+  text-align: center;
+`;
+
+const Singup: FC = () => (
+  <div className={container}>
+    <h2>ToDo Storageにアカウントを作成する</h2>
+    <form>
+      <div>
+        <TextField type="email" placeholder="メールアドレス" />
+      </div>
+      <div>
+        <TextField type="password" placeholder="パスワード" />
+      </div>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/login"
+        >
+          アカウントを作成
+        </Button>
+      </div>
+    </form>
+  </div>
+);
 
 export default Singup;
