278eb8757d5a339adcb8aaf30efcd3c96b6de2fa
KituneUdon <kituneudon97@gmail.com>
2021-03-31 19:04:11 +0900

    #12 todoの完了機能を作成

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,20 +2,60 @@ import React, { FC } from 'react';
 
 import ToDoElement from './ToDoElement';
 
-type Tasks = {
+import firebase from '../config/Firebase';
+
+type Task = {
   id: string;
   task: string;
 };
 
 type Props = {
-  tasks: Tasks[];
+  tasks: Task[];
+  setTasks: (task: Task[]) => void;
+  setErrorMessage: (errorMessage: string) => void;
 };
 
-const ToDoList: FC<Props> = ({ tasks }) => (
-  <>
-    {tasks.map((task) => (
-      <ToDoElement task={task.task} key={task.id} />
-    ))}
-  </>
-);
+const db = firebase.firestore();
+
+const ToDoList: FC<Props> = ({ tasks, setTasks, setErrorMessage }) => {
+  const taskFinish = (task: Task) => {
+    const oldTasks = [...tasks];
+    const newTasks = oldTasks.filter((t) => t.id !== task.id);
+    setTasks(newTasks);
+
+    const uid = firebase.auth().currentUser?.uid;
+    db.collection('tasks')
+      .doc(uid)
+      .collection('finishTodo')
+      .doc(task.id)
+      .set({
+        task: task.task,
+      })
+      .catch(() => {
+        setErrorMessage(
+          'ToDoの追加に失敗しました。時間をおいて再度実行してください。',
+        );
+      });
+
+    db.collection('tasks')
+      .doc(uid)
+      .collection('todo')
+      .doc(task.id)
+      .delete()
+      .catch(() => {
+        setErrorMessage(
+          'ToDoの完了に失敗しました。時間をおいて再度実行してください。',
+        );
+      });
+  };
+
+  return (
+    <>
+      {tasks.map((task) => (
+        <ToDoElement task={task} taskFinish={taskFinish} key={task.id} />
+      ))}
+    </>
+  );
+};
+
 export default ToDoList;
