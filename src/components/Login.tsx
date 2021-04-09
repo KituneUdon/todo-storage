d378b3a29d3aa4d240806a3540d47cd86d22595e
KituneUdon <kituneudon97@gmail.com>
2021-04-09 16:23:41 +0900

    #41 ページ遷移した際にフォーカスがあるほうが便利な箇所にフォーカスを付与

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -63,6 +63,7 @@ const Login: FC = () => {
             margin="normal"
             onChange={handleEmailChange}
             onKeyDown={handleKeyPress}
+            autoFocus
           />
         </div>
         <div>
